a4fe9d37571b4c18425734e18305c7783fd24272
Vlad <creedence8@bk.ru>
2016-07-14 02:31:12 +0300

    gui

---
+++ forecast/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace forecast
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtext = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cal = new System.Windows.Forms.MonthCalendar();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // rtext
+            // 
+            this.rtext.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.rtext.Location = new System.Drawing.Point(14, 214);
+            this.rtext.Name = "rtext";
+            this.rtext.Size = new System.Drawing.Size(481, 107);
+            this.rtext.TabIndex = 11;
+            this.rtext.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(26, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 23);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Текущая дата";
+            // 
+            // cal
+            // 
+            this.cal.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.cal.Location = new System.Drawing.Point(30, 40);
+            this.cal.Name = "cal";
+            this.cal.TabIndex = 9;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Gainsboro;
+            this.button3.Location = new System.Drawing.Point(272, 145);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(209, 43);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "На месяц";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Gainsboro;
+            this.button2.Location = new System.Drawing.Point(272, 92);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(209, 47);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "На неделю";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Gainsboro;
+            this.button1.Location = new System.Drawing.Point(272, 40);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(209, 46);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "На день";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(272, 194);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.ClientSize = new System.Drawing.Size(518, 333);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.rtext);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cal);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.Text = "Прогноз возникновения пожаров";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtext;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MonthCalendar cal;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ forecast/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace forecast
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+           // DateTime dt = DateTime.ParseExact(btext.Text, "ddMyyyy", null);
+            //rtext.Text += dt.Date.ToShortDateString();
+            //DateTime dt = DateTime.ParseExact(cal.SelectionStart.ToShortDateString().ToString(), "ddMyyyy", null);
+            rtext.Text += cal.SelectionStart.ToOADate() + "\n";
+            textBox1.Text = DateTime.FromOADate(40881).ToShortDateString().ToString();
+            //textBox1.Text = DateTime.FromOADate(cal.SelectionStart.ToOADate()).ToShortDateString().ToString();
+        }
+    }
+}
