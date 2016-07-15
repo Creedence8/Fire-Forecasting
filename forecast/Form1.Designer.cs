6fa59d0954503bfeb44d1a3149a71f16a8d533b8
Vlad <creedence8@bk.ru>
2016-07-15 03:38:38 +0300

    added UAO

--- forecast/Form1.Designer.cs
+++ forecast/Form1.Designer.cs
@@ -34,7 +34,11 @@
             this.button3 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // rtext
@@ -42,26 +46,28 @@
             this.rtext.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.rtext.Location = new System.Drawing.Point(14, 214);
+            this.rtext.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.rtext.Location = new System.Drawing.Point(14, 250);
             this.rtext.Name = "rtext";
-            this.rtext.Size = new System.Drawing.Size(481, 107);
+            this.rtext.ReadOnly = true;
+            this.rtext.Size = new System.Drawing.Size(481, 111);
             this.rtext.TabIndex = 11;
             this.rtext.Text = "";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(26, 8);
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(26, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(110, 23);
+            this.label1.Size = new System.Drawing.Size(78, 20);
             this.label1.TabIndex = 10;
-            this.label1.Text = "Текущая дата";
+            this.label1.Text = "Выбор даты";
             // 
             // cal
             // 
             this.cal.BackColor = System.Drawing.SystemColors.ScrollBar;
-            this.cal.Location = new System.Drawing.Point(30, 40);
+            this.cal.Location = new System.Drawing.Point(30, 38);
             this.cal.Name = "cal";
             this.cal.TabIndex = 9;
             // 
@@ -74,6 +80,7 @@
             this.button3.TabIndex = 8;
             this.button3.Text = "На месяц";
             this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button2
             // 
@@ -84,6 +91,7 @@
             this.button2.TabIndex = 7;
             this.button2.Text = "На неделю";
             this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button1
             // 
@@ -96,20 +104,62 @@
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // textBox1
+            // label2
             // 
-            this.textBox1.Location = new System.Drawing.Point(272, 194);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 12;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 231);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Дата";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(158, 231);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(22, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "АО";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(269, 217);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(201, 13);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Прогнозируемое количество пожаров";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(296, 234);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(14, 13);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "P";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(410, 233);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(52, 13);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Round(P)";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LightSteelBlue;
-            this.ClientSize = new System.Drawing.Size(518, 333);
-            this.Controls.Add(this.textBox1);
+            this.ClientSize = new System.Drawing.Size(518, 373);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.rtext);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.cal);
@@ -132,7 +182,11 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
     }
 }
 
--- forecast/Form1.cs
+++ forecast/Form1.cs
@@ -12,6 +12,17 @@ namespace forecast
 {
     public partial class Form1 : Form
     {
+        public static List<Time_series> create_date_list(DateTime d)
+        {
+            List<Time_series>  flist = new List<Time_series>();
+            flist.Add (new Time_series(d));
+            for (int i=1; i<=32; i++)
+            {
+                flist.Add( new Time_series(d) );
+                d = d.Date.AddDays(1);
+            }
+            return flist;
+        }
         public Form1()
         {
             InitializeComponent();
@@ -19,13 +30,71 @@ namespace forecast
 
         private void button1_Click(object sender, EventArgs e)
         {
-            
-           // DateTime dt = DateTime.ParseExact(btext.Text, "ddMyyyy", null);
-            //rtext.Text += dt.Date.ToShortDateString();
-            //DateTime dt = DateTime.ParseExact(cal.SelectionStart.ToShortDateString().ToString(), "ddMyyyy", null);
-            rtext.Text += cal.SelectionStart.ToOADate() + "\n";
-            textBox1.Text = DateTime.FromOADate(40881).ToShortDateString().ToString();
+            rtext.Text = "";
+            rtext.Text += create_date_list(cal.SelectionStart)[2].dt.Date.ToShortDateString() + "               ЮАО                    " + create_date_list(cal.SelectionStart)[2].value.ToString() + "                     " + Math.Round(create_date_list(cal.SelectionStart)[2].value).ToString() + "\n";
+               
+            //DateTime dt = DateTime.ParseExact("01021997", "ddMyyyy", null);
+            ////rtext.Text += dt.Date.ToShortDateString();
+            ////DateTime dt = DateTime.ParseExact(cal.SelectionStart.ToShortDateString().ToString(), "ddMyyyy", null);
+            //rtext.Text += cal.SelectionStart.ToOADate() + "\n";
             //textBox1.Text = DateTime.FromOADate(cal.SelectionStart.ToOADate()).ToShortDateString().ToString();
+            //rtext.Text += dt.Date.DayOfYear;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            rtext.Text = "";
+            for (int i = 2; i <= 8; i++)
+            {
+                rtext.Text += create_date_list(cal.SelectionStart)[i].dt.Date.ToShortDateString() + "               ЮАО                    " + create_date_list(cal.SelectionStart)[i].value.ToString() + "                     " + Math.Round(create_date_list(cal.SelectionStart)[i].value).ToString() + "\n";
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            rtext.Text = "";
+            for (int i = 2; i <= 32; i++)
+            {
+                rtext.Text += create_date_list(cal.SelectionStart)[i].dt.Date.ToShortDateString() + "               ЮАО                    " + create_date_list(cal.SelectionStart)[i].value.ToString() + "                     " + Math.Round(create_date_list(cal.SelectionStart)[i].value).ToString() + "\n";
+            }
+        }
+    }
+
+    public class Time_series
+    {
+        public DateTime dt;
+        public int dtint;
+        public double value;
+        private const double a = -0.00286;
+        private const double b = 4.02431;
+
+        public Time_series(DateTime d)
+        {
+            dt = d;
+            dtint = d.Date.DayOfYear;
+            //value = Math.Round(a * dtint + b, 0);
+            value = a * dtint + b;
+            if (d.Date.Month % 2 != 0 & d.Date.Day <= 7 & 2 <= d.Date.Day)
+            {
+                value += 3.05 ;
+            }
+            if (d.Date.Month % 2 == 0 & d.Date.Day <= 7 & 3 <= d.Date.Day)
+            {
+                value += 2.8;
+            }
+            if (d.Date.Month % 2 == 0 & d.Date.Day <= 13 & 9 <= d.Date.Day)
+            {
+                value += 3;
+            }
+            if (d.Date.Month == 01 & d.Date.Day == 1)
+            {
+                value += 10;
+            }
+        }
+
+        public string get_info()
+        {
+            return "";
         }
     }
 }
